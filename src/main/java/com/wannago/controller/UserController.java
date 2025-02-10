c11bfa99e7b2f3b495c180a4e23c59130189bfe9
seoseuo <seoseuo@naver.com>
2025-02-10 15:56:26 +0900

    Feature : 이름 변경 기능 추가
    사용자 이름 변경 기능 추가
    
    @Transactional
    @Query 어노테이션 방식 사용

--- src/main/java/com/wannago/controller/UserController.java
+++ src/main/java/com/wannago/controller/UserController.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import lombok.extern.log4j.Log4j2;
+import java.util.Map;
 
+@Log4j2
 @RestController
 @RequestMapping("/users/me")
 public class UserController {
@@ -27,33 +30,38 @@ public class UserController {
         return ResponseEntity.ok(userService.findByUsIdx(userDTO.getUsIdx()));
     }
 
+    // 이름 수정
+    @PatchMapping("/name")
+    public ResponseEntity<String> updateName(@RequestBody Map<String, String> request) {
+        String usName = request.get("usName");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();        
+        UserDTO userDTO = (UserDTO) authentication.getPrincipal();
+        userDTO.setUsName(usName);
+        log.info("updateDTO : {}", userDTO);
+        userService.updateUsNameByUsIdx(userDTO.getUsIdx(), usName);
+        return ResponseEntity.ok("Name updated successfully");
+    }
 
-   // 이름 수정
-   @PatchMapping("/name")
-    public ResponseEntity<String> updateName(@RequestBody UserDTO userDTO) {
-    userService.updateUsNameByUsIdx(userDTO.getUsIdx(), userDTO.getUsName());
-    return ResponseEntity.ok("Name updated successfully");
-}
-
-//
-//    // 비밀번호 변경
-//    @PatchMapping("/me/password")
-//    public ResponseEntity<String> updatePassword(@RequestParam String currentPassword, @RequestParam String newPassword) {
-//        userService.updatePassword(currentPassword, newPassword);
-//        return ResponseEntity.ok("Password updated successfully");
-//    }
-//
-//    // 다녀온 여행지 목록 조회
-//    @GetMapping("/me/travel-history")
-//    public List<String> getTravelHistory() {
-//        return userService.getTravelHistory();
-//    }
-//
-//    // 팔로우 목록 조회
-//    @GetMapping("/me/follow-list")
-//    public List<String> getFollowList() {
-//        return userService.getFollowList();
-//    }
-//}
+    //
+    // // 비밀번호 변경
+    // @PatchMapping("/me/password")
+    // public ResponseEntity<String> updatePassword(@RequestParam String
+    // currentPassword, @RequestParam String newPassword) {
+    // userService.updatePassword(currentPassword, newPassword);
+    // return ResponseEntity.ok("Password updated successfully");
+    // }
+    //
+    // // 다녀온 여행지 목록 조회
+    // @GetMapping("/me/travel-history")
+    // public List<String> getTravelHistory() {
+    // return userService.getTravelHistory();
+    // }
+    //
+    // // 팔로우 목록 조회
+    // @GetMapping("/me/follow-list")
+    // public List<String> getFollowList() {
+    // return userService.getFollowList();
+    // }
+    // }
 
 }
--- src/main/java/com/wannago/repository/UserRepository.java
+++ src/main/java/com/wannago/repository/UserRepository.java
@@ -5,16 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     // usIdx를 통해 User 정보를 가져오는 메서드
     Optional<User> findByUsIdx(int usIdx);
-    User findByUsEmail(String usEmail);
-
 
     // usIdx를 통해 User 정보를 수정하는 메서드
+    @Transactional
     @Modifying
     @Query("UPDATE User u SET u.usName = :usName WHERE u.usIdx = :usIdx")
-    void updateUsNameByUsIdx(int usIdx, String usName);
+    void updateUsNameByUsIdx(@Param("usIdx") int usIdx, @Param("usName") String usName);
+    
 }
