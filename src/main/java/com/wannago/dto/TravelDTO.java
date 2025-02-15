7a09541e54c5a8ea863a873768a1a20378fb80ee
seoseuo <seoseuo@naver.com>
2025-02-15 21:23:43 +0900

    chore : Travel 엔티티 TravelDTO 필드 수정

--- src/main/java/com/wannago/dto/TravelDTO.java
+++ src/main/java/com/wannago/dto/TravelDTO.java
@@ -3,11 +3,9 @@ package com.wannago.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import java.time.LocalDateTime;
 import java.util.Date;
 
-import com.wannago.entity.TravelGroup;
-import com.wannago.entity.LocationSi;
+
 
 @Data
 @NoArgsConstructor
@@ -15,10 +13,14 @@ import com.wannago.entity.LocationSi;
 
 public class TravelDTO {
     private int trIdx;
-    private TravelGroup travelGroup;
+    private int grIdx;
     private Date trStartTime;
     private Date trEndTime;
+    private int ldIdx;
     private int lsIdx;
-    private LocationSi locationSi;
     private Date trCreatedAt;
+    // 여행 삭제 날짜 추가
+    private Date trDeletedAt;
+    // 여행 상태 추가
+    private int trState;
 }
