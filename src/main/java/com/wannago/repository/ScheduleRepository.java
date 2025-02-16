4cd5a3e116dc8c403deaaa040d59a6cebba2490a
seoseuo <seoseuo@naver.com>
2025-02-17 00:19:19 +0900

    chore : 레포지토리 커밋

--- src/main/java/com/wannago/repository/ScheduleRepository.java
+++ src/main/java/com/wannago/repository/ScheduleRepository.java
@@ -2,6 +2,13 @@ package com.wannago.repository;
 
 import com.wannago.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {    
+
+    // trIdx를 포함하는 데이터들을 조회 scDate 오름차순으로 정렬하여서.
+    @Query("SELECT s FROM Schedule s WHERE s.trIdx = :trIdx ORDER BY s.scDate ASC")
+    List<Schedule> findByTrIdx(@Param("trIdx") int trIdx);
 }
--- src/main/java/com/wannago/repository/TravelogueRepository.java
+++ src/main/java/com/wannago/repository/TravelogueRepository.java
@@ -2,6 +2,13 @@ package com.wannago.repository;
 
 import com.wannago.entity.Travelogue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface TravelogueRepository extends JpaRepository<Travelogue, Integer> {
+
+    // trIdx를 포함하는 데이터들을 조회 AND tlState = 1
+    @Query("SELECT t FROM Travelogue t WHERE t.trIdx = :trIdx AND t.tlState = 1")
+    List<Travelogue> findByTrIdx(@Param("trIdx") int trIdx);
 }
