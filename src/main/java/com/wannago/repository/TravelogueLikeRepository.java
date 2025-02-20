535e37e2804adb54e3c379e7937084270501f661
seoseuo <seoseuo@naver.com>
2025-02-20 16:49:16 +0900

    feat : 여행록 리스트에 따른 좋아요 목록 조회 구현

--- src/main/java/com/wannago/repository/TravelogueLikeRepository.java
+++ src/main/java/com/wannago/repository/TravelogueLikeRepository.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wannago.entity.TravelogueLike;
-
 public interface TravelogueLikeRepository extends JpaRepository<TravelogueLike, Integer> {
     // 여행록 좋아요 리스트 가져오기
-    List<TravelogueLike> findByTrIdx(int trIdx);
+    List<TravelogueLike> findByTlIdxIn(List<Integer> tlIdxList);
+    
 }
