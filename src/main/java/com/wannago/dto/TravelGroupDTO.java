4c38fa2ac0c2384dab4412a6550aab4c04e64ad9
seoseuo <seoseuo@naver.com>
2025-03-03 19:47:14 +0900

    chore : TravelGroupDTO 수정
    모임 인원 수
    ㅇㅇ도 여행지 목록 추가

--- src/main/java/com/wannago/dto/TravelGroupDTO.java
+++ src/main/java/com/wannago/dto/TravelGroupDTO.java
@@ -2,6 +2,9 @@ package com.wannago.dto;
 
 import lombok.Data;
 import java.util.Date;
+import java.util.Set;
+import java.util.List;
+
 @Data
 public class TravelGroupDTO {
     private int grIdx;
@@ -11,4 +14,10 @@ public class TravelGroupDTO {
     private Date grDeletedAt;
     // 이미지 추가
     private String grProfile;
+
+    // 인원 수
+    private int grCount;
+
+    // 여행지 수
+    private List<String> grLdList;
 }
